1019e9759c746acf4e528ce75d87ca3fb74a8c98
Cundell66 <134798569+Cundell66@users.noreply.github.com>
2023-08-09 21:33:52 +0100

    app.listen("https://capstone.cundell.com/todolist", function() {   console.log("Server started on port todolost"); });

--- app.js
+++ app.js
@@ -133,6 +133,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen("https://capstone.cundell.com/todolist", function() {
+  console.log("Server started on port todolost");
 });
